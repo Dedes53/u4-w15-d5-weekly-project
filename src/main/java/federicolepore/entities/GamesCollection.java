7131e88a7129160edaa4fc72cc2340554d1f99c6
Dedes <federico.lepore73@gmail.com>
2026-03-20 12:27:51 +0100

    GamesCollection addGame searchById

--- src/main/java/federicolepore/entities/GamesCollection.java
+++ src/main/java/federicolepore/entities/GamesCollection.java
@@ -14,11 +14,28 @@ public class GamesCollection {
     }
 
     //   metodi
-    public void addGames(Games game) {
+    public void addGame(Games game) {
 
+        if (game == null) throw new IllegalArgumentException("Devi provare ad aggiungere un gioco, non aria");
+        boolean idIsDifferent = gamesList.stream().anyMatch(g -> g.getId() == game.getId());
+
+        if (!idIsDifferent)
+            throw new IllegalArgumentException("Nella lista un gioco con id: " + game.getId() + "esiste già.");
 
         gamesList.add(game);
     }
+
+    public Games searchById(long id) {
+
+        for (Games g : gamesList) {
+
+            if (g.getId() == id) return g;
+        }
+
+        System.out.println("Non ci sono giochi corrispondenti ad id: " + id);
+        return null;
+    }
+
 
 }
 
